33ed322bbcb1757cfaa56ff3d350625b92c49a2a
NayChiCho <naychicho.dev@gmail.com>
2023-11-28 15:23:01 +0630

    Complete AdoDotNet Patch with RestApi

--- NaychiDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
+++ NaychiDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using NaychiDotNetCore.RestApi.Models;
 using System.Collections.Generic;
 using System.Data;
@@ -145,27 +146,108 @@ namespace NaychiDotNetCore.RestApi.Controllers
             return Ok(model);
         }
 
-        [HttpDelete("{id}")]
-        public IActionResult DeleteBlog(int id)
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, BlogDataModel blog)
         {
             SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
             connection.Open();
-            string query = @"DELETE FROM [dbo].[Tbl_Blog] WHERE Blog_Id = @Blog_Id";
+
+            string query = "select * from tbl_blog where Blog_Id = @Blog_Id";
+
             SqlCommand cmd = new SqlCommand(query, connection);
             cmd.Parameters.AddWithValue("@Blog_Id", id);
-            int result = cmd.ExecuteNonQuery();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                return NotFound(new { IsSuccess = false, Message = "Data not found@" });
+            }
+
+            string conditions = "";
+            //SqlConnection connection2 = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            SqlCommand cmd2 = new SqlCommand();
+
+            if (!string.IsNullOrEmpty(blog.Blog_Title))
+            {
+                conditions += "[Blog_Title] = @Blog_Title, ";
+                cmd2.Parameters.AddWithValue("@Blog_Title", blog.Blog_Title);
+            }
+            if (!string.IsNullOrEmpty(blog.Blog_Author))
+            {
+                conditions += "[Blog_Author] = @Blog_Author, ";
+                cmd2.Parameters.AddWithValue("@Blog_Author", blog.Blog_Author);
+            }
+            if (!string.IsNullOrEmpty(blog.Blog_Content))
+            {
+                conditions += "[Blog_Content] = @Blog_Content, ";
+                cmd2.Parameters.AddWithValue("@Blog_Content", blog.Blog_Content);
+            }
+            if (conditions.Length > 0)
+            {
+                conditions = conditions.Substring(0, conditions.Length - 2);
+            }
+
+            string queryUpdate = $@"UPDATE [dbo].[Tbl_Blog] SET {conditions} WHERE Blog_Id = @Blog_Id";
+            cmd2.CommandText = queryUpdate;
+            cmd2.Connection = connection;
+            cmd2.Parameters.AddWithValue("@Blog_Id", id);
+            var result = cmd2.ExecuteNonQuery();
+
             connection.Close();
 
             BlogResponseModel model = new BlogResponseModel
             {
                 IsSuccess = result > 0,
-                Message = result > 0 ? "Deleting successful." : "Deleting failed."
+                Message = result > 0 ? "Update successful." : "Update failed.",
+            };
+
+            return Ok(model);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = "select * from tbl_blog where Blog_Id=@Blog_Id";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                var response = new { IsSuccess = false, Message = "No data found." };
+                return NotFound(response);
+            }
+
+            connection.Open();
+
+            query = @"DELETE FROM [dbo].[Tbl_Blog]
+                      WHERE Blog_Id = @Blog_Id";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Blog_Id", id);
+            int result = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            BlogResponseModel model = new BlogResponseModel
+            {
+                IsSuccess = result > 0,
+                Message = result > 0 ? "Deleting Successful." : "Deleting Failed."
             };
             return Ok(model);
         }
-
-
-
-
     }
 }
