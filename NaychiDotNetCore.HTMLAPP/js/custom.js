05608a1f403675b063df72fee5b8014e0f4394c7
Nay Chi Cho <naychicho.dev@gmail.com>
2023-12-04 10:22:42 +0630

    notiflix

--- NaychiDotNetCore.HTMLAPP/js/custom.js
+++ NaychiDotNetCore.HTMLAPP/js/custom.js
@@ -1,69 +1,85 @@
 function successMessage(message) {
-    Swal.fire({
+  Swal.fire({
       title: "Success!",
-      text: message, // give message as a parameter and customize at the other components
+      text: message,
       icon: "success",
-    });
-  }
-  
-  // Warning alertBox from sweetalert
-  function warningMessage(message) {
-    Swal.fire({
+  });
+}
+
+function warningMessage(message) {
+  Swal.fire({
       title: "Warning!",
       text: message,
       icon: "warning",
-    });
-  }
-  
-  // uuid from stackoverflow (uiniversal unique id)
-  function uuidv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
-      (
-        c ^
-        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-      ).toString(16)
-    );
-  }
-  
-  // delete alertBox from sweetalert
-  function confirmMessage(message) {
-    return new Promise((resolve, reject) => {
+  });
+}
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+function confirmMessage(message) {
+  return new Promise((resolve, reject) => {
       Swal.fire({
-        title: "Confirm",
-        text: message,
-        icon: "warning",
-        showCancelButton: true,
+          title: "Confirm",
+          text: message,
+          icon: "warning",
+          showCancelButton: true,
       }).then((result) => {
-        // return result.isConfirmed;
-        resolve(result.isConfirmed);
+          // return result.isConfirmed;
+          resolve(result.isConfirmed)
       });
-    });
-  }
-  
-  function notiflixConfirm(message) {
-    return new Promise((resolve, result) => {
+  });
+}
+
+function confirmMessage2(message) {
+  return new Promise((resolve, reject) => {
       Notiflix.Confirm.show(
-        "Notiflix Confirm",
-        "Do you agree with me?",
-        "Yes",
-        "No",
-        function okCb() {
-          resolve(true);
-        },
-        function cancelCb() {
-          resolve(false);
-        },
-        {}
+          'Confirm',
+          message,
+          'Yes',
+          'No',
+          function okCb() {
+              resolve(true)
+          },
+          function cancelCb() {
+              resolve(false)
+          },
+        
       );
-    });
-  }
-  
-  function clear() {
-    $("#txtUserName").val("");
-    $("#txtUserName").focus();
-  }
-  
-  //Notiflix notification
-  function notiMessage() {
-    Notiflix.Notify.success("Save Successful");
-  }
+  });
+}
+
+function readUser() {
+  $("#tbTbody").html("");
+
+  var users = localStorage.getItem("tbl_user");
+  users = JSON.parse(users);
+
+  let htmlString = "";
+  $.each(users, function (index, value) {
+      console.log(index + ": " + value.UserName);
+
+      htmlString += `
+                    <tr>
+                        <td>
+                            <button type="button" class="btn btn-warning" onclick="editUser('${value.UserId}')">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </button>
+                            <button type="button" class="btn btn-danger" onclick="deleteUser('${value.UserId}')">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </td>
+                        <td>${index + 1}</td>
+                        <td>${value.UserName}</td>
+                    </tr>`;
+  });
+
+  $("#tbTbody").html(htmlString);
+}
+
+function resetFormAndReadUser() {
+  $("#txtUserName").val("");
+  $("#txtUserName").focus();
+  readUser();
+}
