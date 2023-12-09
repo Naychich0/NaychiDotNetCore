7d7ea55ca3a91fc89f9916163ff5175288e4ec1d
Nay Chi Cho <naychicho.dev@gmail.com>
2023-12-09 21:22:54 +0630

    new commit

--- NaychiDotNetCore.MvcApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AEHKLMNSTZDotNetCore.MvcApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
