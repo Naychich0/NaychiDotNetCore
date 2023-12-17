4542016afeed6fdf36b2e7d741ca24a101c111b5
NayChiCho <naychicho.dev@gmail.com>
2023-12-17 21:43:35 +0630

    Complete Pagination & Partial Views

---
+++ NaychichoDotNetCore.MVCApp/Controllers/BlogAjaxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NaychichoDotNetCore.MVCApp.Controllers
+{
+    public class BlogAjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NaychichoDotNetCore.MVCApp/Models/BlogDataModel.cs
+++ NaychichoDotNetCore.MVCApp/Models/BlogDataModel.cs
@@ -19,4 +19,35 @@ namespace NaychichoDotNetCore.MVCApp.Models
         public String Blog_Content { get; set; }
 
     }
+
+    public class BlogDataResponseModel
+    {
+        public PageSettingModel PageSetting { get; set; }
+        public List<BlogDataModel> Blogs { get; set; }
+    }
+
+    public class PageSettingModel 
+    {
+        public PageSettingModel()
+        {
+        }
+
+        public PageSettingModel(int pageNo, int pageSize, int pageCount,string pageUrl) 
+        {
+            PageNo = pageNo;
+            PageCount = pageCount;
+            PageSize = pageSize;
+            PageUrl = pageUrl;
+
+        }
+
+        public int PageNo { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int PageCount { get; set; }
+
+        public string PageUrl { get; set; }
+    }
+
 }
