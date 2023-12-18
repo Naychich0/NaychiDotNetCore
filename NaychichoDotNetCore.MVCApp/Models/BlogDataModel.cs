62ae207660ac5d7c229ac79822cb11a47692cbaa
NayChiCho <naychicho.dev@gmail.com>
2023-12-18 19:13:49 +0630

    complete ajax crud

--- NaychichoDotNetCore.MVCApp/Models/BlogDataModel.cs
+++ NaychichoDotNetCore.MVCApp/Models/BlogDataModel.cs
@@ -50,4 +50,16 @@ namespace NaychichoDotNetCore.MVCApp.Models
         public string PageUrl { get; set; }
     }
 
+    public class MessageModel
+    {
+        public MessageModel() { }
+        public MessageModel(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
 }
